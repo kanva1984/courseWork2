afcc84413fdf4dc5c059aff854a2b91917f78935
kanva1984 <kanva1984@gmail.com>
2023-07-09 12:39:43 +0200

    начало courseWork2

---
+++ src/main/java/com/kanva1984/examinerthing/ExaminerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kanva1984.examinerthing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaminerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kanva1984/examinerthing/ExaminerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanva1984.examinerthing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
